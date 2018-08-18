83ac1f2e923f17acbb83c8ad2abca1ae66250dcf
Liam <liamreadsspam@gmail.com>
2018-08-18 00:15:30 -0300

    Fix config sidebar order

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,39 +1,39 @@
 module.exports = {
 	title: 'A\'therys Scripting',
 	description: 'Documentation for the A\'therys Scripting plugin',
-	base: "/docs",
+	base: "/docs/",
 	themeConfig: {
 		nav: [
+			{text: 'Quests', link: '/quests/'},
 			{text: 'A\'therys', link: 'https://atherys.com'},
-			{
 		],
 		sidebar: {
-			'/': [
-				 '/A-bear-bones-introduction-to-JavaScript',
-				 '/Functions',
-				 '/Block-Functions',
-				 '/Damage-Functions',
-				 '/Entity-Functions',
-				 '/Event-Functions',
-				 '/Item-Functions',
-				 '/Location-Functions',
-				 '/Particle-Functions',
-				 '/Player-Functions',
-				 '/Potion-Functions',
-				 '/Sound-Functions',
-				 '/Task-Functions',
-				 '/Text-Functions',
-				 '/Utility-Functions',
-				 '/The-srun-Command'
-			],
-			'/quests/' : [
+			'/quests/': [
 				 'Dialog',
 				 'Events',
 				 'Objectives',
 				 'Quest-Creation',
 				 'Requirements',
 				 'Retrieval',
-				 'Rewards',
+				 'Rewards'
+			],
+			'/': [
+				 'A-bear-bones-introduction-to-JavaScript',
+				 'Functions',
+				 'Block-Functions',
+				 'Damage-Functions',
+				 'Entity-Functions',
+				 'Event-Functions',
+				 'Item-Functions',
+				 'Location-Functions',
+				 'Particle-Functions',
+				 'Player-Functions',
+				 'Potion-Functions',
+				 'Sound-Functions',
+				 'Task-Functions',
+				 'Text-Functions',
+				 'Utility-Functions',
+				 'The-srun-Command'
 			]
 		},
 		repo: 'Atherys-Horizons/AtherysScript'
