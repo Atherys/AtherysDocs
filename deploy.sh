46be8affc893ad6a9aded070592f3775a46c1793
Liam <liamreadsspam@gmail.com>
2018-09-05 18:13:00 -0300

    Get rid of log in deploy

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 # Run from master branch with clean state
 echo "Building site..."
 cd docs
-vuepress build > lastlog.log 2> error.log
+vuepress build > /dev/null 2> /dev/null
 echo "Moving files..."
-mkdir ../../temp/ >> lastlog.log 2> /dev/null
-rm -R ../../temp/* >> lastlog.log
-mv .vuepress/dist/* ../../temp/ >> lastlog.log
+mkdir ../../temp/ >> /dev/null 2> /dev/null
+rm -R ../../temp/* >> /dev/null
+mv .vuepress/dist/* ../../temp/ >> /dev/null
 git checkout gh-pages > /dev/null
 cd ..
-rm -Rf * >> lastlog.log
-git rm * >> lastlog.log
-mv ../temp/* . >> lastlog.log
-rm -R ../temp/ >> lastlog.log
+rm -Rf * >> /dev/null
+git rm * >> /dev/null
+mv ../temp/* . >> /dev/null
+rm -R ../temp/ >> /dev/null
 echo "Committing..."
-git add * >> lastlog.log
-git commit -m 'Deploy' >> lastlog.log
+git add * >> /dev/null
+git commit -m 'Deploy' >> /dev/null
 echo "Pushing..."
 git push origin gh-pages
