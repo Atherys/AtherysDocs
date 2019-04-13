548a546ed4eaeea405bcc1160d39a6852bf75a4b
HaedHutner <mvvasilev@outlook.com>
2019-04-13 13:48:54 +0300

    Improve skill graph editor styling

--- docs/.vuepress/components/RpgSkillEditor.vue
+++ docs/.vuepress/components/RpgSkillEditor.vue
@@ -1,19 +1,18 @@
 <template>
-   <div id="container">
-   <div v-if="editingNode" id="network-popUp">
-      <span id="operation">node</span>
-      <div id="node-editor">
+   <div id="rpg-container">
+
+      <div v-if="editingNode" id="network-pop-up">
+         <span id="operation">node</span>
+         <div id="node-editor"></div>
+         <button @click="">Save</button>
+         <button @click="editingNode = false">Cancel</button>
       </div>
-      <button @click="">
-         Save
-      </button>
-      <button @click="editingNode = false">
-         Cancel
-      </button>
+
+      <div id="skill-tree">
       </div>
-   <div id="skill-tree">
-   </div>
-   <input type="file" @change="readSkillTree">
+
+      <input type="file" @change="readSkillTree">
+      
    </div>
 </template>
 
@@ -71,18 +70,26 @@ export default {
    methods: {
       buildSkillTree() {
          const element = document.getElementById('skill-tree');
+
          let data = {
             nodes: this.skillNodes,
             edges: this.skillLinks
          };
+
          let network = new Vis.Network(element, data, options);
       },
+
       cancelEdit(callback) {
          callback(null);
       },
+
       readSkillTree(e) {
          const files = e.target.files || e.dataTransfer.files;
-         if (!files.length) { return; }
+
+         if (!files.length) { 
+            return; 
+         }
+
          const skillTreeFile = files[0];
          const reader = new FileReader();
          let data = null;
@@ -90,26 +97,56 @@ export default {
          reader.onload = (e) => {
             this.loadSkillTree(reader.result);
          }
+
          reader.readAsText(skillTreeFile);
       },
+
       loadSkillTree(hocon) {
          let skillTree = Hocon(hocon);
+
          if (skillTree['skill-graph']) {
+
+            let lastUsedColor = 0x000000;
             let nodesObject = skillTree['skill-graph']['skill-nodes'];
             let nodes = [];
+
             Object.keys(nodesObject).forEach( (key) => {
                const node = nodesObject[key];
-               console.log(key, node);
-               nodes.push(Object.assign(node, {id: key, label: node['skill-id']})) 
-            })
+               nodes.push(Object.assign(node, {
+                  id: key, 
+                  label: node['skill-id'],
+                  shape: 'box',
+                  shadow: {
+                     enabled: true
+                  },
+                  color: {
+                     background: '#aa3300',
+                     border: '#000000'
+                  },
+                  font: {
+                     size: 18
+                  }
+               }));
+            });
 
             let edges = skillTree['skill-graph']['skill-links '];
+
             edges.forEach( (edge) => {
                edge.from = edge['parent-node'];
                edge.to = edge['child-node'];
-            })
+
+               if (edge.type == "UNIDIRECTIONAL") {
+                  edge.arrows = 'to';
+               } else {
+                  edge.arrows = 'to, from';
+               }
+
+               edge.smooth = false;
+            });
+
             this.skillNodes = new Vis.DataSet(nodes);
             this.skillLinks = new Vis.DataSet(edges);
+
             this.buildSkillTree();
          }
       }
@@ -117,32 +154,38 @@ export default {
 }
 </script>
 
-<style <style lang="stylus">
-#network-popUp 
-   position:absolute;
-   top:350px;
-   left:170px;
-   z-index:299;
-   width:250px;
-   height:120px;
-   background-color: #f9f9f9;
-   border-style:solid;
-   border-width:3px;
-   border-color: #5394ed;
-   padding:10px;
-   text-align: center;
+<style lang="stylus">
+#skill-tree
+   height 75vh
+
+#network-pop-up 
+   position:absolute
+   top 350px
+   left 170px
+   z-index 299
+   width 250px
+   height 120px
+   background-color #f9f9f9
+   border-style solid
+   border-width 3px
+   border-color #5394ed
+   padding 10px
+   text-align center
+
 .vis-network
-   position:relative;
-   width: 800px;
-   height: 600px;
-   border: 1px solid lightgray;
-   font-size:28px;
+   position relative
+   width 800px
+   height 600px
+   border 1px solid lightgray
+   font-size 28px
+
 .vis-button 
    font-size 16px
    margin-left 10px
+
 .vis-manipulation
    display flex !important
-   border-top 1px solid lightgray;
+   border-top 1px solid lightgray
    height 35px
    align-items center
 </style>
