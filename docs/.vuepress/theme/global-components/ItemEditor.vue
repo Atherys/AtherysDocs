dcbd3c83cd500cbe4db5dd258bc9880cab487d68
Liam <liamreadsspam@gmail.com>
2020-03-22 14:00:36 -0300

    Implement item list, add hide flags

--- docs/.vuepress/theme/global-components/ItemEditor.vue
+++ docs/.vuepress/theme/global-components/ItemEditor.vue
@@ -1,56 +1,75 @@
 <template>
    <div id="item-editor">
-      <div id="options">
-         <fieldset id="general-options">
-            <legend> Item Options </legend>
-            <BasicOption id="ID" v-model="item.id"/>
-            <BasicOption id="Name" v-model="item.name"/>
-            <BasicOption id="Durability" v-model.number="item.durability" type="number"/>
-            <BasicOption id="Quantity" v-model.number="item.quantity" type="number"/>
-            <BasicOption id="Item Type" v-model="item.type"/>
-            <div class="option">
-               <div class="label">Lore</div>
-               <textarea rows="2" v-model.lazy="item.lore"></textarea>
-            </div>
-         </fieldset>
+      <div id="editor">
+         <div id="item-options">
+            <fieldset id="general-options">
+               <legend> Item Options </legend>
+               <BasicOption id="ID" v-model="items[selectedItem].id"/>
+               <BasicOption id="Name" v-model="items[selectedItem].name"/>
+               <BasicOption id="Durability" v-model.number="items[selectedItem].durability" type="number"/>
+               <BasicOption id="Quantity" v-model.number="items[selectedItem].quantity" type="number"/>
+               <BasicOption id="Item Type" v-model="items[selectedItem].type"/>
+               <div class="option">
+                  <div class="label">Hide Flags</div>
+                  <input type="checkbox" v-model="items[selectedItem]['hide-flags']">
+               </div>
+               <div class="option lore">
+                  <div class="label">Lore</div>
+                  <textarea rows="2" v-model.lazy="items[selectedItem].lore"></textarea>
+               </div>
+            </fieldset>
 
-         <fieldset id="attributes">
-            <legend> Attributes </legend>
-            <div class="attribute" v-for="(amount, name) in item.attributes">
-                  <div class="label">{{ name | capitalize }}</div>
-                  <input class="attribute-input" v-model.number.lazy="item.attributes[name]" type="number" step="any">
-            </div>
-         </fieldset>
+            <fieldset id="attributes">
+               <legend> Attributes </legend>
+               <div class="attribute" v-for="(amount, name) in items[selectedItem].attributes">
+                     <div class="label">{{ getDisplayName(name) }}</div>
+                     <input class="attribute-input" v-model.number.lazy="items[selectedItem].attributes[name]" type="number" step="any">
+               </div>
+            </fieldset>
 
-         <fieldset id="enchantments">
-            <legend> Enchantments </legend>
-            <div class="enchantment" v-for="enchantment in item.enchantments"> 
-               <div>
-                  <div class="label"> Type </div>
-                  <input v-model.lazy="enchantment.type"> 
+            <fieldset id="enchantments">
+               <legend> Enchantments </legend>
+               <div id="enchantments-list">
+                  <div class="enchantment" v-for="enchantment in items[selectedItem].enchantments"> 
+                     <div>
+                        <div class="label"> Type </div>
+                        <input v-model.lazy="enchantment.type"> 
+                     </div>
+                     <div>
+                        <div class="label"> Level </div> 
+                        <input class="level" type="number" v-model.number.lazy="enchantment.level">
+                     </div>
+                  </div>
                </div>
                <div>
-                  <div class="label"> Level </div> 
-                  <input class="level" type="number" v-model.number.lazy="enchantment.level">
+                  <button @click="addEnchantment">
+                     Add
+                  </button>
+                  <button @click="removeEnchantment">
+                     Remove
+                  </button>
                </div>
+            </fieldset>
+         </div>
+         <fieldset id="item-list">
+            <legend> Items </legend>
+            <ul id="item-list-element">
+               <template v-for="(item, index) in items">
+                  <li :id="'item-' + index" class="item-element">
+                     <div class="item" @click="selectItem(index)"> {{ item.name + " (" + item.id + ")"}} </div>
+                     <button class="delete-button" @click="deleteItem(index)"> x </button>
+                  </li>
+               </template>
+            </ul>
+            <div id="item-list-controls">
+               <button @click="addItem"> Add Item </button>
             </div>
-            <button @click="addEnchantment">
-               Add Enchantment
-            </button>
-            <button @click="removeEnchantment">
-               Remove Enchantment
-            </button>
          </fieldset>
       </div>
-
-      <button @click="generate">Generate Item</button>
       <button @click="clearAll">Clear All</button>
-      <button @click="downloadJson">Download</button>
-      <button @click="copy">Copy Text</button>
-
-      <div class="output-window">
-         <pre>{{ generated }}</pre>
-      </div>
+      <button @click="downloadJson">Download Items</button>
+      <button @click="clickUpload">Upload Items</button>
+      <input type="file" @change="readItems" id="upload-config">
 
       <a id="download"></a>
       <textarea id="copy"></textarea>
@@ -61,6 +80,18 @@
 import { downloadFile } from '../util/util';
 import BasicOption from '../components/BasicOption';
 
+const DISPLAY_NAMES = {
+   "atherys:constitution": "Constitution",
+   "atherys:strength": "Strength",
+   "atherys:dexterity": "Dexterity",
+   "atherys:intelligence": "Intelligence",
+   "atherys:wisdom": "Wisdom",
+   "atherys:magical_resistance": "Magical Res",
+   "atherys:physical_resistance": "Physical Res",
+   "atherys:base_armor": "Base Armor",
+   "atherys:base_damage": "Base Damage"
+};
+
 export default {
    components: {
       BasicOption
@@ -68,9 +99,76 @@ export default {
 
    data () {
       return {
-         item: {
-            id: "",
-            name: "",
+         items: [this.createDefaultItem()],
+         selectedItem: 0
+      }
+   },
+   mounted() {
+      this.selectItem(0);
+   },
+   methods: {
+      downloadJson() {
+         downloadFile("items=" + JSON.stringify(this.items, this.replacer, 3), "items.conf", "download");
+      },
+      readItems(e) {
+         const files = e.target.files || e.dataTransfer.files;
+
+         if (!files.length) { 
+            return; 
+         }
+         const reader = new FileReader();
+         reader.onload = (e) => {
+            this.items = JSON.parse(reader.result).items;
+         }
+         reader.readAsText(files[0]);
+      },
+      clickUpload() {
+         document.getElementById("upload-config").click();
+      },
+      replacer(key, value) {
+         if (value === 0 || value === "") {
+            return undefined;
+         }
+         return value;
+      },
+      clearAll() {
+         Object.assign(this.$data, this.$options.data.call(this));
+      },
+      copy() {
+         let copied = document.getElementById('copy');
+         copied.value = this.generated;
+         copied.select();
+         let bool = document.execCommand("copy");
+         console.log(bool);
+      },
+      addEnchantment() {
+         this.items[this.selectedItem].enchantments.push({type:"", level:""});
+      },
+      removeEnchantment() {
+         if (this.items[this.selectedItem].enchantments.length > 0) {
+            this.items[this.selectedItem].enchantments.pop();
+         }
+      },
+      addItem() {
+         this.items.push(this.createDefaultItem());
+      },
+      deleteItem(index) {
+         if (this.items.length === 1) {
+            this.addItem();  
+         } else if (index <= this.selectedItem && index !== 0) {
+            this.selectItem(this.selectedItem - 1);
+         }
+         this.items.splice(index, 1);
+      },
+      selectItem(index) {
+         document.getElementById("item-" + this.selectedItem).classList.remove("item-selected");
+         this.selectedItem = index;
+         document.getElementById("item-" + index).classList.add("item-selected");
+      },
+      createDefaultItem() {
+         return {
+            id: "new-item",
+            name: "New Item",
             durability: "",
             quantity: 1,
             type: "",
@@ -86,89 +184,12 @@ export default {
                "atherys:base_armor": 0,
                "atherys:base_damage": 0
             },
+            "hide-flags": false,
             enchantments: []
-         },
-         generated: ""
-      }
-   },
-   methods: {
-      downloadJson () {
-         downloadFile(json, this.item.id + ".json", "download");
-      },
-      generate () {
-         let info = "{\n";
-         Object.keys(this.item).forEach(option => {
-            if (option === "enchantments" || option === "attributes") {
-               info += this.optionToString(option, this.item[option]);
-            } else if(this.item[option]) {
-               info += "\t" + option + this.optionToString(option, this.item[option]) + "\n";
-            }
-         });
-         info += "}";
-         this.generated = info;
-      },
-      optionToString(option, value) {
-         let prepared = "\t";
-         switch(option) {
-            case "name": case "type":
-               prepared = "=\"" + value + "\"";
-               break;
-            case "id":
-               prepared = "=" + value.split(' ').join('-');
-               break;
-            case "lore":
-               prepared = "=[";
-               value.split('\n').forEach(line => {
-                  prepared += "\n\t\t\"" + line + "\",";
-               })
-               prepared = prepared.replace(/.$/,"");
-               prepared += "\n\t]";
-               break;
-            case "enchantments":
-               prepared = "";
-               value.forEach(enchantment => {
-                  prepared += '\n\t\t"' + enchantment.type + '"=' + enchantment.level + ",";
-               });
-               prepared = prepared.replace(/.$/, "");
-               if (prepared === "") {
-                  prepared = "";
-               } else {
-                  prepared = "\tenchantments=[" + prepared + "\n\t]\n";
-               }
-               break;
-            case "attributes":
-               prepared = "";
-               Object.keys(value).forEach(attribute => {
-                  if (value[attribute]) {
-                     prepared += "\t\t" + attribute + "=" + value[attribute] + "\n";
-                  }
-               })
-               if (prepared !== "") {
-                  prepared = "\tattributes {\n" + prepared + "\t}\n";
-               }
-               break;
-            default:
-               prepared = "=" + value;
          }
-         return prepared;
       },
-      clearAll () {
-         Object.assign(this.$data, this.$options.data.call(this));
-      },
-      copy() {
-         let copied = document.getElementById('copy');
-         copied.value = this.generated;
-         copied.select();
-         let bool = document.execCommand("copy");
-         console.log(bool);
-      },
-      addEnchantment() {
-         this.item.enchantments.push({type:"", level:""});
-      },
-      removeEnchantment() {
-         if (this.item.enchantments.length > 0) {
-            this.item.enchantments.pop();
-         }
+      getDisplayName(attribute) {
+         return DISPLAY_NAMES[attribute];
       }
    },
    filters: {
@@ -198,6 +219,43 @@ textarea
    max-width: 100%
    box-sizing: border-box
 
+#editor 
+   display: flex
+   margin-bottom: 10px
+
+#item-list
+   width: 40%
+   display: flex
+   flex-direction: column
+   justify-content: space-between
+
+ul
+   list-style-type: none
+   height: 470px
+   overflow: auto
+
+.item-element
+   display: flex
+   justify-content: space-between
+   align-items: center
+   margin-bottom: 10px
+   border-bottom: solid 1px
+   padding-bottom: 2px
+
+.item
+   cursor: pointer
+
+.delete-button
+   background-color: #E64F4F
+   border: none
+   color: white
+   cursor: pointer
+   border-radius: 5px
+   height: 20px
+
+.item-selected 
+   color: #e2950f
+
 .output-window 
    color: wheat
 
@@ -215,16 +273,21 @@ textarea
    display: flex
    align-items: center
    margin-right: 5px
-   width: 30%
+   width: 20%
    min-width: 60px
 
-#enchantments 
-   padding-right: 20px
+#enchantments
+   display: flex
+   flex-direction: column
+
+#enchantments-list
+   height: 225px
+   overflow: auto
 
 .enchantment
-   display: grid 
-   grid-template-columns: 1fr 1fr
+   border-bottom: solid 1px
    margin-bottom: 10px
+   padding-bottom: 5px
    > .label
       min-width: 50px
       max-width: 50px
@@ -232,8 +295,8 @@ textarea
       margin-right: 10px
    > div
       display: flex
-   > div:last-of-type 
-      margin-left: 10px
+   > div:first-child
+      margin-bottom: 5px
 
 .level
    max-width: 50px
@@ -245,21 +308,29 @@ textarea
 .attribute .label 
    min-width 100px
 
-#options 
+#item-options 
    display: grid 
-   grid-template-columns: 1fr 1fr
+   grid-template-columns: 1fr
    grid-gap: 10px
-   margin-bottom: 10px
+   width: 60%
+   margin-right: 10px
 
 #general-options 
    display: grid
    grid-template-columns: repeat(2, 1fr)
+   grid-column: 1 / span 2
    grid-gap: 10px
    > div
       margin-bottom: 5px
-      > input, textarea
-         width: 150px
+      > input[type="number"]
+         width: 75px
          box-sizing: border-box
+      > input
+         width: 200px
+
+textarea 
+   width: 100%
+   height: 75px
 
 #attributes 
    display: grid
@@ -269,6 +340,9 @@ textarea
 .attribute-input 
    width: 10%
    min-width: 50px
+
+#upload-config 
+   display none
 
 @media (max-width: 1100px)
    #options
